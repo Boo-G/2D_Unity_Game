9a4ad760552d2c3e581ce736b0936da1f245d1b4
Bailey Giesbrecht (bcg009) <bcg009@mail.usask.ca>
2024-01-06 20:41:14 -0600

    first commit

---
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private Animator animate;
+    private PlayerMovementScript playerMovement ;
+    private float cooldownTimer = Mathf.Infinity; // make it so that the timer doesnts start at zero
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animate = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovementScript>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && cooldownTimer > attackCooldown && playerMovement.canAttack()){
+            Attack();
+        }
+
+        cooldownTimer += Time.deltaTime;
+        
+    }
+
+    private void Attack(){
+        animate.SetTrigger("attack");
+        cooldownTimer = 0;
+
+        // object pooling: create invisable objects that become visable upon attack
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<ProjectileScript>().SetDirection(Mathf.Sign(transform.localScale.x));
+
+
+    }
+
+    private int FindFireball(){
+        // check all the fireballs to find one that is not active so we can use it to fire more than one fireball
+        for(int i = 0; i < fireballs.Length; i++){
+            if(!fireballs[i].activeInHierarchy){
+                return i;
+            }
+        }
+
+        return 0;
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpPower;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    private Rigidbody2D body;
+    private Animator animate;
+    private BoxCollider2D boxCollider;
+    private float wallJumpCooldown;
+    private float horizontalInput;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get regerences from game object
+        body = GetComponent<Rigidbody2D>();
+        animate = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Variables
+        horizontalInput = Input.GetAxis("Horizontal");
+
+ 
+
+        // Set animate parameters
+        animate.SetBool("run",horizontalInput != 0);
+        animate.SetBool("grounded",isGrounded());
+
+
+        // Wall Jump Logic
+        if (wallJumpCooldown > 0.2f){
+
+            body.velocity = new Vector2(horizontalInput*speed, body.velocity.y);
+
+        
+
+            if(onWall() && !isGrounded()){
+                body.gravityScale = 0;
+                body.velocity = new Vector2(0,0);
+            }
+            else{
+                body.gravityScale = 3;
+            }
+
+            if(Input.GetKey(KeyCode.Space)){
+                Jump();
+            }
+
+        }
+        else{
+            wallJumpCooldown += Time.deltaTime;
+        }
+
+        // flip the player sprite when moving to the left or right
+        if(horizontalInput > 0.01){
+            transform.localScale = new Vector3(1,1,1);
+        }
+        else if(horizontalInput < -0.01){
+            transform.localScale = new Vector3(-1,1,1);
+        }
+
+        
+    }
+
+    // Jump function
+    private void Jump(){
+        // regular jump
+        if(isGrounded()){
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            animate.SetTrigger("jump");
+        }
+
+        // wall jump
+        else if(onWall() && !isGrounded()){
+            //idk if i like this if, adds push off of wall when not holding a direction
+            if(horizontalInput == 0){
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x)*10,0) ;
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+
+
+            }
+            else{
+                // create a force opposite of where the player is facing on the wall and push them up
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x)*3,6) ;
+
+            }
+            wallJumpCooldown = 0;
+    
+        }
+
+    }
+
+
+    // create a box that checks 0.1 units under the player to see if there is a groundLayer
+    // if something is under the player, raycastHit.collider != null evaluates to True, else False
+    private bool isGrounded(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    
+    private bool onWall(){
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x,0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+
+    public bool canAttack(){
+        return horizontalInput == 0 && isGrounded() && !onWall();
+    }
+}
