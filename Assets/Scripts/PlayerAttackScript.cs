3134243399e155a546d586ebc4335d7dad839dbf
Bailey Giesbrecht (bcg009) <bcg009@mail.usask.ca>
2024-01-08 11:48:05 -0600

    test

--- Assets/Scripts/PlayerAttackScript.cs
+++ Assets/Scripts/PlayerAttackScript.cs
@@ -11,7 +11,7 @@ public class PlayerAttackScript : MonoBehaviour
     private PlayerMovementScript playerMovement ;
     private float cooldownTimer = Mathf.Infinity; // make it so that the timer doesnts start at zero 
 
-    // test
+    // test on bailey branch
 
     // Start is called before the first frame update
     void Start()
